b8eaeae9fc2db743b8295ade26af818e5b35edd5
tarun-t <thammisettytarun@gmail.com>
2016-10-06 20:26:21 +0530

    corrected boot_alloc

--- kern/pmap.c
+++ kern/pmap.c
@@ -154,7 +154,7 @@ mem_init(void)
 	// array.  'npages' is the number of physical pages in memory.  Use memset
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
-
+	pages = (struct PageInfo*) boot_alloc(npages*sizeof(struct PageInfo));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
